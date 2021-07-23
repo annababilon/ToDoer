f152ba8c56cc7fd98fc10bac0dfa185207eef732
annadotka <anna.dotka@gmail.com>
2021-07-23 22:59:28 +0200

    Refactored code

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ export default function Header({todosAmount}) {
             </div>
 
             <SummaryPanel todosAmount={todosAmount}/>
-            <LoginPanel></LoginPanel>
+            <LoginPanel/>
         </div>
     )
 }
